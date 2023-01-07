9ba108c056621128192da91f22ca1d9a7966661d
colorfultrees <colorfultrees@users.noreply.github.com>
2023-01-07 23:21:20 +0100

    cloud management added

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -5,14 +5,43 @@ class Cloud extends MoveableObject {
     height = 0;
     speed; // The speed in ms at which the cloud moves
     distance = 0.5; // The distance which the cloud moves at each step
+    intervalId = 0;
 
     constructor(positionX, positionY) {
         super(positionX, positionY).loadImage(this.imgUrls[Math.round(Math.random())]);
+        this.setDimensions();
+        this.setSpeed();
+        this.setCreationInterval();
+    }
+
+    
+    setDimensions() {
         this.width = calcRandomNumber(200, 500);
         this.height = this.width / this.aspectRatio;
+    }
+
+
+    setSpeed() {
         this.speed = calcRandomNumber(35, 90);
-        setInterval(() => {
-            this.move(-1, this.distance);
-        }, this.speed);
+    }
+
+
+    setCreationInterval() {
+        this.intervalId = setInterval(() => {
+                this.move(-1, this.distance);
+                this.manageIntervalsClouds();
+            }, this.speed);
+    }
+
+
+    manageIntervalsClouds() {
+        const posRight = this.positionX + this.width;
+        if (posRight < 0) {
+            const cloudId = world.background.clouds.findIndex(c => c === this);
+            clearInterval(this.intervalId);
+            world.background.clouds.splice(cloudId, 1);
+
+            console.log(`cloudInterval(${this.intervalId}) cleared, clouds[${cloudId}] removed.`);
+        }
     }
 }
