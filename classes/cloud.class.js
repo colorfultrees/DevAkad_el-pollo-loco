c8abd60dd428d313be56b47892de8a8ceb4c6b77
colorfultrees <colorfultrees@users.noreply.github.com>
2023-01-15 20:21:47 +0100

    bug-fix _clouds not rendered_

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -2,7 +2,7 @@ class Cloud extends MoveableObject {
     imgUrls = ['./img/5_background/layers/4_clouds/1.png', './img/5_background/layers/4_clouds/2.png'];
 
     constructor(positionX, positionY) {
-        super(positionX, positionY); //.loadImage(this.imgUrls[Math.round(Math.random())]);
+        super(positionX, positionY).loadImage(this.imgUrls[Math.round(Math.random())]);
         this.aspectRatio = 1.7778;
         this.setDimensions();
         this.movingDistance = 0.5;
